7bb25d27cacfbb9f5515fbf2a7adea4ddef4e5b9
Aaron Martin <akatzgager@gmail.com>
2025-12-31 02:26:09 -0500

    quick fix

--- apps/web/app/(public)/grease-trap-cleaning/page.tsx
+++ apps/web/app/(public)/grease-trap-cleaning/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { Badge } from "../../../components/ui/badge";
 import { Button } from "../../../components/ui/button";
 import { Card, CardContent, CardHeader } from "../../../components/ui/card";
